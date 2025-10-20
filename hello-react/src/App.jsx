53e23f21f7327dda2c8f5a354136259f1707b4d1
Abhishek-Sirugudu <siruguduabhishek@gmail.com>
2025-10-20 17:22:12 +0530

    feat: Initial commit of first React app

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = 'Abhishek';
+  return (
+    <>
+    <h1>Hello React</h1>
+    <p>This is my first application, built with Vite</p>
+    <p>My name is {name}</p>
+    </>
+  )
+}
+
+export default App
